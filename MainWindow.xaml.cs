1277075b4d0e106bdb994b824ac43a340f3ca604
elliotnash <elliotnash@users.noreply.github.com>
2020-05-09 23:23:58 -0700

    Now detects miinecraft folder... Added config too

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Configuration;
 using System.Diagnostics;
+using System.IO;
+using System.Threading;
 
 namespace eLOITsModSync
 {
@@ -25,8 +27,98 @@ namespace eLOITsModSync
         public MainWindow()
         {
             InitializeComponent();
-            currentDirectoryTextField.Text = ConfigurationManager.AppSettings["minecraftDirectory"];
+
+            
+
+            if (getValue("firstRun").Equals("true"))
+            {
+                String minecraftDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.minecraft";
+                Debug.WriteLine(minecraftDirectory);
+                storeValue("minecraftDirectory", minecraftDirectory);
+                storeValue("firstRun", "false");
+
+                currentDirectoryTextField.Text = minecraftDirectory;
+                OpenFolder(minecraftDirectory);
+            }
+            else
+            {
+                currentDirectoryTextField.Text = getValue("minecraftDirectory");
+                OpenFolder(getValue("minecraftDirectory"));
+            }
+
+            
+        }
+
+
+
+
+
+
+
+        
+
+        public static void storeValue(String key, String value)
+        {
+            try
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+                config.AppSettings.Settings.Remove(key);
+                config.AppSettings.Settings.Add(key, value);
+                config.Save(ConfigurationSaveMode.Minimal);
+            }catch (Exception e)
+            {
+
+            }
+        }
+        public static String getValue(String key)
+        {
+            try
+            {
+                return ConfigurationManager.AppSettings.Get(key);
+            }catch(Exception e) { 
+            }
+            return null;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void OpenFolder(string folderPath)
+        {
+            if (Directory.Exists(folderPath))
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    Arguments = folderPath,
+                    FileName = "explorer.exe"
+                };
+                Process.Start(startInfo);
+            }
+            else
+            {
+                MessageBox.Show(string.Format("{0} Directory does not exist!", folderPath));
+            }
         }
     }
-
 }
+
+
+
+
+
