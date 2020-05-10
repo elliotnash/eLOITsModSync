fac34caa8012792991d9291bf7a94aef048ded60
elliotnash <elliotnash@users.noreply.github.com>
2020-05-10 10:00:12 -0700

    Added change directories and stuff

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,6 +16,10 @@ using System.Configuration;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.CodeDom;
+using Microsoft.VisualBasic;
+
 
 namespace eLOITsModSync
 {
@@ -24,39 +28,38 @@ namespace eLOITsModSync
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Initializes variables from config so they're public
+        public static String minecraftDirectory;
+        public static String downloadAddressString;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            
 
+
+            //Gets the default minecraft directory if first run, else gets the value from config.
             if (getValue("firstRun").Equals("true"))
             {
-                String minecraftDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.minecraft";
+                minecraftDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.minecraft";
                 Debug.WriteLine(minecraftDirectory);
                 storeValue("minecraftDirectory", minecraftDirectory);
                 storeValue("firstRun", "false");
-
-                currentDirectoryTextField.Text = minecraftDirectory;
-                OpenFolder(minecraftDirectory);
             }
             else
             {
-                currentDirectoryTextField.Text = getValue("minecraftDirectory");
-                OpenFolder(getValue("minecraftDirectory"));
+                minecraftDirectory = getValue("minecraftDirectory");
             }
-
             
+            //Don't pull values from config once running, only save them
+            //Sets current dirctory text block to current minecraft directory
+            currentDirectoryTextField.Text = minecraftDirectory;
+
+            downloadAddressString = getValue("downloadAddress");
+
+
         }
-
-
-
-
-
-
-
-        
-
+        //pushes changes to config file
         public static void storeValue(String key, String value)
         {
             try
@@ -71,6 +74,7 @@ namespace eLOITsModSync
 
             }
         }
+        //reads a value from config
         public static String getValue(String key)
         {
             try
@@ -80,25 +84,7 @@ namespace eLOITsModSync
             }
             return null;
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //Opens a folder with selected folder path
         private void OpenFolder(string folderPath)
         {
             if (Directory.Exists(folderPath))
@@ -114,6 +100,41 @@ namespace eLOITsModSync
             {
                 MessageBox.Show(string.Format("{0} Directory does not exist!", folderPath));
             }
+        }
+
+        private void changeDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            //asks for .minecraft directory
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = minecraftDirectory;
+            dialog.IsFolderPicker = true;
+            dialog.Title = "Select Minecraft directory";
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                minecraftDirectory = dialog.FileName;
+                storeValue("minecraftDirectory", minecraftDirectory);
+                currentDirectoryTextField.Text = minecraftDirectory;
+            }
+        }
+
+        private void resetDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Are you sure you would like to reset the minecraft directory to %AppData%\\.minecraft?", "Reset Minecraft Directory", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                minecraftDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.minecraft";
+                storeValue("minecraftDirectory", minecraftDirectory);
+                currentDirectoryTextField.Text = minecraftDirectory;
+
+            }
+        }
+
+        private void changeAddress_Click(object sender, RoutedEventArgs e)
+        {
+            inputDialog inputDialog = new inputDialog("Please enter a link to a direct download of a .zip file containing only folder with a minecraft directory. all files inside that folder will be copied to inside the .minecraft folder", downloadAddressString);
+            if (inputDialog.ShowDialog() == true)
+                downloadAddressString = inputDialog.Answer;
+
         }
     }
 }
